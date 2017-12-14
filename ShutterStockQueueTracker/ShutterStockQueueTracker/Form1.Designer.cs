ae6b582ba0cbaae320ecb8a37e2921dedcfbef8e
ashishwithV2 <ashish.dubey@V2Solutions.com>
2017-12-14 16:06:58 +0530

    first commit

---
+++ ShutterStockQueueTracker/ShutterStockQueueTracker/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ShutterStockQueueTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1338, 720);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1330, 694);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "URL Browser";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(670, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Track Every";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(742, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(799, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "min (s)";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(38, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1238, 557);
+            this.panel1.TabIndex = 5;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1216, 547);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(858, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(563, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(77, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "https://review.shutterstock.com/";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1330, 694);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Error Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(3, 16);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1293, 646);
+            this.textBox2.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 744);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ShutterStock Queue Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ ShutterStockQueueTracker/ShutterStockQueueTracker/Form1.cs
@@ -0,0 +1,464 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Reflection;
+
+namespace ShutterStockQueueTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         string StopRecords = "";
+        //Start & Stop Button
+     
+           
+         private  void button2_Click(object sender, EventArgs e)
+        {
+
+            if (webBrowser1.DocumentText != "")
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(comboBox1.Text, "^[0-9]*$"))
+                {
+                    MessageBox.Show("Invalid time duration?");
+                }
+                else
+                {
+                   
+                    if (button2.Text == "Start")
+                    {
+                        button2.Text = "Stop";
+                        button2.BackColor = Color.Tan;
+                        StopRecords = "";
+                        //timer1.Tick += new EventHandler(timer1_Tick);
+                        timer1.Interval = Convert.ToInt32(comboBox1.Text) * 60 * 1000;
+                        timer1.Enabled = true;
+                      
+                    }
+                    else
+                    {
+                        timer1.Enabled = false;
+                        StopRecords = "Stop";
+                        button2.Text = "Start";
+                        button2.BackColor = Color.Transparent;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Wait.. Webbrowser is not fully loaded.");
+            }
+          
+            
+
+
+        }
+        // Form Load Event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.AutoSize = true;
+            combostyle();
+            WebBroswerBind();
+            string name = "ssqt.ico";
+            var exeDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var imgDir = Path.Combine(exeDir, name);
+            System.Drawing.Icon ico = new System.Drawing.Icon(imgDir);
+            this.Icon = ico;
+
+        }
+        // Combobox binding method
+        private void combostyle()
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDown;
+           
+            comboBox1.Items.Clear();
+            comboBox1.SelectedText = "30";
+            comboBox1.Items.Add(1);
+            comboBox1.Items.Add(2);
+            comboBox1.Items.Add(3);
+            comboBox1.Items.Add(4);
+            comboBox1.Items.Add(5);
+            comboBox1.Items.Add(10);
+            comboBox1.Items.Add(15);
+            comboBox1.Items.Add(20);
+            comboBox1.Items.Add(25);
+            comboBox1.Items.Add(30);
+            comboBox1.Items.Add(35);
+            comboBox1.Items.Add(40);
+            comboBox1.Items.Add(45);
+            comboBox1.Items.Add(50);
+            comboBox1.Items.Add(55);
+            comboBox1.Items.Add(60);
+           
+        }
+        // Binding webbrowser method 
+        private void WebBroswerBind()
+        {
+            var appName = System.IO.Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            Microsoft.Win32.Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION",
+                  appName, 11000, Microsoft.Win32.RegistryValueKind.DWord);
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                webBrowser1.AllowNavigation = true;
+                webBrowser1.ScriptErrorsSuppressed = true;
+              
+                webBrowser1.Navigate(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Enter URL!!");
+            }
+        }
+        // Go Button Code here 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBroswerBind();
+        }
+        // request with google speedsheet and bind the records
+        private string GetValues(string photo, string largecollection, string photoip, string illustrationip, string vectorip, string bigstockphoto)
+        {
+            
+          //  var script_url = "https://script.google.com/macros/s/AKfycbxEEAvSi51zKzeVfrqHBt0siUpykrmbcGbUuvM2n_5LP4c1hsI/exec"; // old jitendra shutterstocl google Excel sheet click
+            var script_url = "https://script.google.com/macros/s/AKfycbzU8UC-MBtITMNq9RiNXes2SWLoeGfVe6DRLSgH5a9_ux7Qtss/exec";
+            
+            int _min = 1000;
+            int _max = 99999999;
+            Random _rdm = new Random();
+            int id1 = _rdm.Next(_min, _max);
+
+            var url = script_url + "?callback=ctrlq&id=" + id1 + "&photo=" + photo + "&largecollection=" + largecollection + "&photoip=" + photoip + "&illustrationip=" + illustrationip + "&vectorip=" + vectorip + "&bigstockphoto=" + bigstockphoto + "&action=insert";
+
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "GET";
+            String test = String.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                test = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+            return test;
+        }
+
+        // Fetch
+        private void Fetchrecords()
+        {
+            string photo = "";
+            string largecollection = "";
+            string photoip = "";
+            string illustrationip = "";
+            string vectorip = "";
+            string bigstockphoto = "";
+            this.webBrowser1.Navigate(textBox1.Text);
+            // fetching data from webbrowser control
+            string rec = webBrowser1.DocumentText;
+            
+
+
+            var HtmlDoc = new HtmlAgilityPack.HtmlDocument();
+            HtmlDoc.LoadHtml(rec);
+          
+            string phto = "";
+            string phto1 = "";
+            string phto2 = "";
+            string phto3 = "";
+            string phto4 = "";
+            string phto5 = "";
+
+          
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+               Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    string td = Convert.ToString(cell.InnerText);
+
+                    if ((td.Trim().ToLower()) == "photo")
+                    {
+                        PhotoTotal++;
+
+                        phto = "photo";
+
+
+
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto == "photo" && PhotoTotal == 3)
+                    {
+                        photo = Convert.ToString(td);
+                    }
+                    
+                }
+
+            }
+
+            // photo
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+                Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    string td = Convert.ToString(cell.InnerText);
+
+                    //large collection
+                    if ((td.Trim().ToLower()) == "large collection")
+                    {
+                        PhotoTotal++;
+                        phto1 = "large collection";
+
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto1 == "large collection" && PhotoTotal == 3)
+                    {
+                        largecollection = Convert.ToString(td);
+                    }
+                   
+                }
+            }
+
+
+            // photo
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+               Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    string td = Convert.ToString(cell.InnerText);
+
+                    //photo ip
+                    if ((td.Trim().ToLower()) == "photo ip")
+                    {
+                        PhotoTotal++;
+                        phto2 = "photo ip";
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto2 == "photo ip" && PhotoTotal == 3)
+                    {
+                        photoip = Convert.ToString(td);
+                    }
+                }
+            }
+
+
+            // illustration ip
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+               Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    string td = Convert.ToString(cell.InnerText);
+                    //illustration ip
+                    if ((td.Trim().ToLower()) == "illustration ip")
+                    {
+                        PhotoTotal++;
+
+                        phto3 = "illustration ip";
+
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto3 == "illustration ip" && PhotoTotal == 3)
+                    {
+                        illustrationip = Convert.ToString(td);
+                    }
+                }
+            }
+
+            //  //Vector Ip
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+                Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    
+                    string td = Convert.ToString(cell.InnerText);
+
+                    //Vector Ip
+                    if ((td.Trim().ToLower()) == "vector ip")
+                    {
+                        PhotoTotal++;
+
+                        phto4 = "vector ip";
+
+
+
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto4 == "vector ip" && PhotoTotal == 3)
+                    {
+                        vectorip = Convert.ToString(td);
+                    }
+
+                }
+            }
+
+            //  //Vector Ip
+            foreach (var row in HtmlDoc.DocumentNode.SelectNodes("//table[@id = 'queues_table']/tbody/tr"))
+            {
+                Application.DoEvents();
+                int PhotoTotal = 0;
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                
+                    string td = Convert.ToString(cell.InnerText);
+
+                    //Bigstock Photo
+                    if ((td.Trim().ToLower()) == "bigstock photo")
+                    {
+                        PhotoTotal++;
+
+                        phto5 = "bigstock photo";
+                    }
+
+                    else
+                    {
+                        if (PhotoTotal == 1 || PhotoTotal == 2)
+                        {
+                            PhotoTotal++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+                    if (phto5 == "bigstock photo" && PhotoTotal == 3)
+                    {
+                        bigstockphoto= Convert.ToString(td);
+                    }
+
+                }
+            }
+    
+            // getting data from different source and sent back to google speedsheet
+            GetValues(photo, largecollection, photoip, illustrationip, vectorip, bigstockphoto);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+               //if (StopRecords == "Stop")
+               // {
+               //     timer.Stop();
+               //     timer.Enabled = false;
+               // }
+               // else
+               // {
+               //     Fetchrecords();
+               // }
+
+                Fetchrecords();
+
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = (DateTime.Now.ToString() + " " + ex.Message);
+               
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                 this.Hide();
+            }
+          
+            
+        }
+
+
+
+    }
+}
